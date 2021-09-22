0b98531b7f0fe20cc4b4d921da3f4c35a4095ab9
xudean <xuda.it@outlook.com>
2021-09-22 16:31:36 +0800

    IU-2021.2.2 <xudean@xudean-pc Create baseRefactoring.xml, ide.general.xml, rainbow_brackets.xml, Enum.java, Interface.java, shared-indexes.xml, Default.xml, visualizationTool.xml, keymap.xml, find.xml, ui.lnf.xml, filetypes.xml, colors.scheme.xml, scala_config.xml, databaseDrivers.xml, Record.java, laf.xml, Default.xml, scala.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, vcs.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author  xudean@matrixelements.com
+ * @date  ${DATE} ${TIME}
+ */
+public enum ${NAME} {
+}
